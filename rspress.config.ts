12298da5902b8e7e7530a0fadab0e27ab5027f47
Shane <shane@swalker.dev>
2024-10-24 11:51:14 -0500

    added pluginPreview

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,18 +1,39 @@
-import * as path from 'path';
-import { defineConfig } from 'rspress/config';
+import * as path from "path";
+import { defineConfig } from "rspress/config";
+import { withZephyr } from "zephyr-webpack-plugin";
+import { pluginPreview } from "@rspress/plugin-preview";
+
+const zephyrRsbuildPlugin = () => ({
+  name: "zephyr-rsbuild-plugin",
+  // @ts-ignore
+  setup(api) {
+    //@ts-ignore - todo figure this out
+    api.modifyRspackConfig((rspackConfig) => {
+      return withZephyr()(rspackConfig);
+    });
+  }
+});
 
 export default defineConfig({
-  root: path.join(__dirname, 'docs'),
-  title: 'Docs Demo',
-  description: 'Rspack-based Static Site Generator',
-  icon: '/rspress-icon.png',
+  root: path.join(__dirname, "docs"),
+  title: "Docs Demo",
+  description: "Rspack-based Static Site Generator",
+  icon: "/rspress-icon.png",
   logo: {
-    light: '/rspress-light-logo.png',
-    dark: '/rspress-dark-logo.png',
+    light: "/rspress-light-logo.png",
+    dark: "/rspress-dark-logo.png"
   },
   themeConfig: {
     socialLinks: [
-      { icon: 'github', mode: 'link', content: 'https://github.com/web-infra-dev/rspress' },
-    ],
+      {
+        icon: "github",
+        mode: "link",
+        content: "https://github.com/web-infra-dev/rspress"
+      }
+    ]
   },
+  builderConfig: {
+    plugins: [zephyrRsbuildPlugin()]
+  },
+  plugins: [pluginPreview({ defaultRenderMode: "pure" })]
 });
